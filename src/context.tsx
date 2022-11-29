5a3e9215408c1b77e8a2a7d58b89e21c84828bb8
Denis <brokenhandq3@yandex.ru>
2022-11-29 15:27:19 +0300

    44 context without ts

--- src/context.tsx
+++ src/context.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import {RootStateType} from "./redux/state";
 
+//const AppContext = React.createContext({} as RootStateType)
 const AppContext = React.createContext(null)
 
+type AppProviderProps = {
+    store: RootStateType
+    children: React.ReactNode
+}
 
+export const AppProvider = (props: any) => {
+    return <AppContext.Provider value={props.store}>
+        {props.children}
+    </AppContext.Provider>
+}
+
+export default AppContext
--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,22 @@ import App from './App';
 
 import RouterComp from "./components/TestRouter/RouterComp";
 import store from './redux/redux-store'
+import AppContext from "./context";
 
 let rerenderEntireTree = () => ReactDOM.render(<App store={store}/>, document.getElementById('root'));
 
 rerenderEntireTree();
 
 store.subscribe(() => rerenderEntireTree());
+
+// let rerenderEntireTree = (store:any) => {
+//     ReactDOM.render(
+//         <AppContext.Provider value={store}>
+//             <App/>
+//         </AppContext.Provider>, document.getElementById('root'));
+// }
+//
+// rerenderEntireTree(store);
+//
+// store.subscribe(() => rerenderEntireTree(store));
 
